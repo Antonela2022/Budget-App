463f9c350ceb2477a38a864424081d1260545f99
Antonela2022 <103432614+Antonela2022@users.noreply.github.com>
2023-07-02 16:20:11 +0300

    UI designs migrated totally

---
+++ app/src/main/java/eu/ase/ro/planificareabugetuluipersonal/AdaugaObiectiveActivity.kt
@@ -0,0 +1,11 @@
+package eu.ase.ro.planificareabugetuluipersonal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdaugaObiectiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adauga_obiective)
+    }
+}
