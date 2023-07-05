02dbcd7be2cf3ff46f54ad4051077aee071dfc07
sebastianncraciun <craciunsebastian52@gmail.com>
2023-07-05 11:11:21 +0300

    login si signup facute

--- app/src/main/java/eu/ase/ro/planificareabugetuluipersonal/SignUpActivity.kt
+++ app/src/main/java/eu/ase/ro/planificareabugetuluipersonal/SignUpActivity.kt
@@ -1,11 +1,42 @@
 package eu.ase.ro.planificareabugetuluipersonal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import eu.ase.ro.planificareabugetuluipersonal.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+//        binding.textView.setOnClickListener {
+//            val intent = Intent(this, SignInActivity::class.java)
+//            startActivity(intent)
+//        }
+        binding.signUpBtnInregistrare.setOnClickListener {
+            val email = binding.signUpTietEmail.text.toString()
+            val pass = binding.signUpTietParola.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, LogInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
