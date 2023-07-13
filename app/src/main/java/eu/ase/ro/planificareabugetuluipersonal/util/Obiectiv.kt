e1e772aff55ad563f29750596952a56904f0bb12
Antonela2022 <103432614+Antonela2022@users.noreply.github.com>
2023-07-13 20:52:43 +0300

    fragment obiective implementat total

---
+++ app/src/main/java/eu/ase/ro/planificareabugetuluipersonal/util/Obiectiv.kt
@@ -0,0 +1,7 @@
+package eu.ase.ro.planificareabugetuluipersonal.util
+
+data class Obiectiv(
+    val numeObiectiv: String,
+    val status: String,
+    val valoareObiectiv: Double,
+)
---
+++ app/src/main/java/eu/ase/ro/planificareabugetuluipersonal/util/ObiectivAdapter.kt
@@ -0,0 +1,38 @@
+package eu.ase.ro.planificareabugetuluipersonal.util
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import eu.ase.ro.planificareabugetuluipersonal.R
+
+class ObiectivAdapter( private val obiectiveList: ArrayList<Obiectiv>) :
+    RecyclerView.Adapter<ObiectivAdapter.ObiectivViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ObiectivViewHolder {
+        val itemView=
+            LayoutInflater.from(parent.context).inflate(R.layout.lv_row_obiective_item,parent,false)
+        return ObiectivViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ObiectivViewHolder, position: Int) {
+        val currentItem=obiectiveList[position]
+        holder.numeObiectiv.text=currentItem.numeObiectiv
+        holder.status.text= currentItem.status.toString()
+        holder.valoareObiectiv.text=currentItem.valoareObiectiv.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return obiectiveList.size
+    }
+
+    class ObiectivViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val numeObiectiv: TextView = itemView.findViewById(R.id.row_item_tv_obiective_nume)
+        val status: TextView = itemView.findViewById(R.id.row_item_tv_obiective_status)
+        val valoareObiectiv: TextView = itemView.findViewById(R.id.row_item_obiective_valoare)
+
+
+    }
+}
